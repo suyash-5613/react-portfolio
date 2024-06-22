035f3e868f8b32c9a8fe89bb137b7c0d3f8ad37a
suyash-5613 <suyashsahu0405@gmail.com>
2024-06-22 23:58:20 +0530

    added about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,25 +7,27 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className='pb-8'>
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
         <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora lusto! Blanditiis error
-          iste, totan fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+          Hi, I'm Suyash Sahu, a passionate and experienced web developer with a
+          knack for creating dynamic and responsive web applications. With a
+          strong foundation in both front-end and back-end technologies, I
+          strive to deliver seamless and user-friendly digital experiences.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          earum error commodi pariatur velit libero minus sint nisi repudiandae
-          fugit vel sequi magni nesciunt, accusamus quod suscipit ad dicta!
-          Quisquam!
+          My expertise spans both front-end and back-end development, leveraging
+          technologies such as HTML, CSS, JavaScript, React, Node.js, and
+          MongoDB. I've had the privilege of working on a diverse range of
+          projects, from high-traffic e-commerce platforms to sleek personal
+          portfolios. My focus is on creating intuitive user experiences and
+          writing clean, efficient code that meets modern web standards.
         </p>
       </div>
     </div>
